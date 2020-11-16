8dd3bcb0af22f38568baf84ae392f55eb0dd937b
kiedia <jirka.kiebzak@gmail.com>
2020-11-16 16:49:57 +0100

    fingerprint authentication

--- app/src/main/java/com/example/serpensortia/MainActivity.java
+++ app/src/main/java/com/example/serpensortia/MainActivity.java
@@ -1,8 +1,20 @@
 package com.example.serpensortia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +22,62 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView msg_text = findViewById(R.id.txt_msg);
+        Button login_btn = findViewById(R.id.login_btn);
+
+        BiometricManager biometricManager = BiometricManager.from(this);
+        switch (biometricManager.canAuthenticate()){
+            case BiometricManager.BIOMETRIC_SUCCESS:
+                msg_text.setText("Můžete použít senzor pro přihlášení");
+                msg_text.setTextColor(Color.parseColor("#Fafafa"));
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                msg_text.setText("Zařízení nedisponuje senzorem otisku prstu");
+                login_btn.setVisibility(View.GONE);
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                msg_text.setText("Senzor otisků prstů není dostupný");
+                login_btn.setVisibility(View.GONE);
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                msg_text.setText("Zařízení nemá uložený otisk prstu, prosím nastavte si otisk pro přístup do aplikace");
+                login_btn.setVisibility(View.GONE);
+                break;
+        }
+
+        final Executor executor = ContextCompat.getMainExecutor(this);
+        final BiometricPrompt biometricPrompt = new BiometricPrompt(MainActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(getApplicationContext(), "Úspěšné přihlášení", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+            }
+        });
+
+        final BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("LOGIN")
+                .setDescription("Použite otisk prstu pro přihlášení do aplikace")
+                .setNegativeButtonText("zavřít")
+                .build();
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                biometricPrompt.authenticate(promptInfo);
+            }
+        });
+
+
     }
 }
