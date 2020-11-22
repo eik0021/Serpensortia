69cbf0a0847d0c0efa28de8118a17763676580fb
kiedia <jirka.kiebzak@gmail.com>
2020-11-22 10:34:42 +0100

    sugar orm setup

---
+++ app/src/main/java/com/example/serpensortia/configs/Sugar.java
@@ -0,0 +1,18 @@
+package com.example.serpensortia.configs;
+
+import com.orm.SugarApp;
+import com.orm.SugarContext;
+
+public class Sugar extends SugarApp {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SugarContext.init(getApplicationContext());
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        SugarContext.terminate();
+    }
+}
